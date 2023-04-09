e8e94ce0849d53430665cc2f3a4c48f2023a6dab
Ryan Hasbie <105176413+ryanhasbie@users.noreply.github.com>
2023-04-09 22:58:47 +0700

    add Admin model

---
+++ src/main/java/com/ryan/managementlibrary/models/Admin.java
@@ -0,0 +1,36 @@
+package com.ryan.managementlibrary.models;
+
+public class Admin {
+    private Integer adminId;
+    private String username;
+    private String password;
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
